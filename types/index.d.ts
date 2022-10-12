c1270934e29bfdb6cc3bbb1b822ddc2f5933ffba
hanchenchen <hanchenchen@tydic.com>
2022-10-12 12:03:53 +0800

    添加类型

---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for iview 3.3.1
+// Project: https://github.com/iview/iview
+// Definitions by: yangdan
+// Definitions: https://github.com/yangdan8/iview.git
+import iViewProUi from './iview';
+
+export default iViewProUi;
+export as namespace iViewProUi;
+
+export * from './iview.components';
---
+++ types/iview.d.ts
@@ -0,0 +1,6 @@
+import { PluginFunction } from 'vue';
+
+interface IViewProUi extends PluginFunction<any> {}
+
+declare const iViewProUi: IViewProUi;
+export default iViewProUi;
