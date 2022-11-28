a19e68a674e9877f361eceb25e1d4f1948e1823a
hanchenchen <hanchenchen@tydic.com>
2022-11-28 11:37:03 +0800

    优化

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import TablePage from './src/components/TablePage.vue';
 import SearchForm from './src/components/SearchForm.vue';
 import PageTitle from './src/components/PageTitle.vue';
 
-import copy from './src/directives/copy';
+// import copy from './src/directives/copy';
 import loading from './src/directives/loading';
 
 const iViewProUi = {
   install(Vue) {
-    Vue.directive('copy', copy);
+    // Vue.directive('copy', copy);
     Vue.directive('loading', loading);
 
     Vue.component('TablePage', TablePage);
